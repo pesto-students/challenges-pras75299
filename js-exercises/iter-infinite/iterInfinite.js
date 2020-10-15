cf62ce3336004156036b72936622c1534bbfee1c
pras75299 <singh.prashantking@gmail.com>
2020-10-15 18:51:30 +0530

    solve iter-infinite question

---
+++ js-exercises/iter-infinite/iterInfinite.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-param-reassign */
+function checkValidNumber(value) {
+  if (typeof value !== 'number' || Number.isNaN(value)) {
+    throw TypeError(`Expected of type number, instead got type of ${typeof value}`);
+  }
+}
+
+function checkIterator(value) {
+  if (typeof value[Symbol.iterator] !== 'function') {
+    throw TypeError(`Expected an Iterator got ${typeof value}`);
+  }
+}
+
+//! Count Function
+function* count(start, step = 1) {
+  checkValidNumber(start);
+  checkValidNumber(step);
+  while (true) {
+    yield (start);
+    start += step;
+  }
+}
+
+//! cycle Function
+function* cycle(iter, n) {
+  checkIterator(iter);
+  checkValidNumber(n);
+  while (n--) {
+    for (const el of iter) {
+      yield el;
+    }
+  }
+}
+
+// repeat Function
+function* repeat(value, n) {
+  checkValidNumber(n);
+  while (n) {
+    yield value;
+  }
+}
+
+export {
+  count,
+  cycle,
+  repeat,
+};
