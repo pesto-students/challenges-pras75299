49941644ed2c6f2d67b62f90fe5ef6d2cded1622
pras75299 <singh.prashantking@gmail.com>
2020-10-21 19:47:24 +0530

    add remove button to list

--- react-exercises/grocery-list-react/src/List.js
+++ react-exercises/grocery-list-react/src/List.js
@@ -6,8 +6,9 @@ const List = (props) => (
     <h1>Grocery List Items</h1>
     <ul className="unorderGrocery-list">
       {props.items.map((item, index) => (
-        <Card>
+        <Card key={index}>
           <Card.Body key={index}>
+            <a class="remove">x</a>
             <Card.Title>Item Name: <span>{item}</span></Card.Title>
           </Card.Body>
         </Card>        
